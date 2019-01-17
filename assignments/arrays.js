8e5c803d6721a20b1a10b78957ccdf28b52701dd
Edgar F <eflores.ef@gmail.com>
2019-01-16 20:24:45 -0800

    Started working on stretch tasks.

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -101,7 +101,6 @@ console.log(carYears);
 // ==== Challenge 5 ====
 // The car lot manager needs to find out how many cars are older than the year 2000. Using the carYears array you just created, find out how many cars were made before the year 2000 by populating the array oldCars and logging it's length.
 let oldCars = [];
-let newCars = [];
 for (let i = 0; i < carYears.length; i++) {
     if (carYears[i] < 2000) {
         oldCars.push(carYears[i]);
